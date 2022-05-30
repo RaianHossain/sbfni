77e8ba6fcfbde1254f6dffd6285032424f308b64
santo1989 <65486217+santo1989@users.noreply.github.com>
2022-05-30 18:32:34 +0600

    Footer and copy right should be changed. #7

--- resources/views/components/frontend/layouts/partials/footer.blade.php
+++ resources/views/components/frontend/layouts/partials/footer.blade.php
@@ -1,101 +1,81 @@
-<footer class="section page-footer">
+  <footer class="section page-footer">
     <div class="novi-background bg-cover bg-default">
-      <div class="shell-wide">
-        <div class="hr bg-gray-light"></div>
-      </div>
-      <div class="section-60">
-        <div class="shell">
-          <div class="range range-50 range-lg-justify range-xs-center">
-            <div class="cell-md-3 cell-lg-3">
-              <a class="reveal-inline-block" href="index.html"><img src="images/logo-default-2-144x122-1.png" alt="" srcset="images/logo-default-2-144x122-1.png" /></a>
-              <div class="offset-top-30 text-center">
-                <ul class="list-inline list-inline-xs list-inline-madison">
-                  <li>
-                    <a class="icon novi-icon icon-xxs fa-facebook icon-circle icon-gray-light-filled" href="#"></a>
-                  </li>
-                  <li>
-                    <a class="icon novi-icon icon-xxs fa-twitter icon-circle icon-gray-light-filled" href="#"></a>
-                  </li>
-                  <li>
-                    <a class="icon novi-icon icon-xxs fa-google icon-circle icon-gray-light-filled" href="#"></a>
-                  </li>
-                  <li>
-                    <a class="icon novi-icon icon-xxs fa-instagram icon-circle icon-gray-light-filled" href="#"></a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div class="cell-xs-10 cell-md-5 cell-lg-4 text-lg-left">
-              <h6 class="text-bold">Contact us</h6>
-              <div class="text-subline mb-4"></div>
-              <div class="offset-top-30">
-                <ul class="list-unstyled contact-info list">
-                  <li>
-                    <div class="unit unit-horizontal unit-middle unit-spacing-xs">
-                      <div class="unit-left">
-                        <span class="icon novi-icon mdi mdi-phone text-middle icon-xs text-madison"></span>
-                      </div>
-                      <div class="unit-body">
-                        <a class="text-dark" href="tel:#">01716934024,</a><a class="reveal-block reveal-md-inline-block text-dark" href="tel:#">01716934024</a>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="unit unit-horizontal unit-middle unit-spacing-xs">
-                      <div class="unit-left">
-                        <span class="icon novi-icon mdi mdi-map-marker text-middle icon-xs text-madison"></span>
-                      </div>
-                      <div class="unit-body text-left">
-                        <a class="text-dark" href="#">SBF Nursing Institute,<br>Sonali Park, Jail Road,<br>Lalmonirhat-5500, Bangladesh.</a>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="unit unit-horizontal unit-middle unit-spacing-xs">
-                      <div class="unit-left">
-                        <span class="icon novi-icon mdi mdi-email-open text-middle icon-xs text-madison"></span>
-                      </div>
-                      <div class="unit-body">
-                        <a href="mailto:#" style="color:#5F9EA0;">sbfnil@gmail.com</a>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div class="cell-xs-10 cell-md-8 cell-lg-4 text-lg-left">
-              <h6 class="text-bold">Newsletter</h6>
-              <div class="text-subline mb-3"></div>
-              <div class="offset-top-30 text-left">
-                <p class="mb-3">
-                  Enter your email address to get the latest 
-                  news, special events and student activities delivered
-                  right to your inbox.
-                </p>
-              </div>
-              <div class="offset-top-10">
-                <form class="rd-mailform form-subscribe" data-form-output="form-output-global" data-form-type="subscribe" method="post" action="bat/rd-mailform.php">
-                  <div class="form-group">
-                    <div class="input-group input-group-sm">
-                      <label class="form-label" for="form-email">Your e-mail</label><input class="form-control" id="form-email" type="email" name="email" data-constraints="@Required @Email" /><span class="input-group-btn"><button class="btn btn-default btn-sm" style="background-color:#999999" type="submit">
-                          Subscribe
-                        </button></span>
-                    </div>
-                  </div>
-                  <div class="form-output"></div>
-                </form>
-              </div>
-            </div>
-          </div>
+        <div class="shell-wide">
+            <div class="hr bg-gray-light"></div>
         </div>
-      </div>
-      <div class="section-5 context-dark" style="background-color:#002833;">
-        <div class="shell text-md-left">
-          <p class="">
-            © <span class="copyright-year">2022</span> All Rights Reserved
-            Terms of Use and <a href="privacy.html">Privacy Policy.</a><span> Developed&nbsp;by Break-IT</span>
-          </p>
+        <div class="section-60">
+            <div class="shell">
+                <div class="range range-50 range-lg-justify range-xs-center">
+                    <div class="cell-md-3 cell-lg-3 text-center"><a class="reveal-inline-block"  href="http://www.sbfus.org" target="_blank" ><img src="{{ asset('ui/frontend/images/logo/sbf.png') }}" alt="" width="191" height="80" srcset="{{ asset('ui/frontend/images/logo/sbf.png') }}"><br /><h5>Sonar Bangla Foundation (SBF)</h5></a>
+                        <div class="offset-top-30 text-center">
+                            <ul class="list-inline list-inline-xs list-inline-madison">
+                                <li><a class="icon novi-icon icon-xxs fa-facebook icon-circle icon-gray-light-filled" href="https://www.facebook.com/SBF-Nursing-Institute-lalmonirhat-495775934160351" target="blank"></a></li>
+                                <li><a class="icon novi-icon icon-xxs fa-twitter icon-circle icon-gray-light-filled" href="#"></a></li>
+                                <li><a class="icon novi-icon icon-xxs fa-google icon-circle icon-gray-light-filled" href="#"></a></li>
+                                <li><a class="icon novi-icon icon-xxs fa-youtube icon-circle icon-gray-light-filled" href="https://www.youtube.com/sonarbanglafoundation" target="blank"></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="cell-xs-10 cell-md-5 cell-lg-4 text-lg-left">
+                        <h6 class="text-bold">Contact us</h6>
+                        <div class="text-subline"></div>
+                        <div class="offset-top-30">
+                            <ul class="list-unstyled contact-info list">
+                                <li>
+                                    <div class="unit unit-horizontal unit-middle unit-spacing-xs">
+                                        <div class="unit-left"><span class="icon novi-icon mdi mdi-phone text-middle icon-xs text-madison"></span></div>
+                                        <div class="unit-body"><a class="text-dark" href="tel:+8801318045241">+8801318045241,</a><a class="reveal-block reveal-md-inline-block text-dark" href="tel:+8801747613298">+8801747613298</a></div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="unit unit-horizontal unit-middle unit-spacing-xs">
+                                        <div class="unit-left"><span class="icon novi-icon mdi mdi-map-marker text-middle icon-xs text-madison"></span></div>
+                                        <div class="unit-body text-left"><a class="text-dark" href="#">SBF Nursing Institute,<br>Sonali Park, Jail Road,<br>Lalmonirhat-5500,  Bangladesh.</a></div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="unit unit-horizontal unit-middle unit-spacing-xs">
+                                        <div class="unit-left"><span class="icon novi-icon mdi mdi-email-open text-middle icon-xs text-madison"></span></div>
+                                        <div class="unit-body"><a href="mailto:sbfnil@gmail.com">sbfnil@gmail.com</a></div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="cell-xs-10 cell-md-8 cell-lg-4 text-lg-left" style="padding: 10px 10px">
+
+                        <h6 class="text-bold" > Adopt a student in SBFNI</h6>
+                        <div class="text-subline" style="color:black ">SBF is running a Nursing Institute at Lalmonirhat to help rural students engaged in noble nursing profession. It provides quality education and practical training to make the financially needy students stand on their own feet. The students are either orphans or come of an extreme needy family. You may adopt such a student by sponsoring his/her tuition, logging, food etc.
+
+                        </div>
+
+                        <li class="col-sm-3">
+                
+                                <!--======= LINK ========= -->  
+                                <br><br>
+                                <br><br>
+                                <br><br>
+                                <br><br>
+                                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
+                                <input type="hidden" name="cmd" value="_s-xclick">
+                                <input type="hidden" name="hosted_button_id" value="3P8DDJKCZUY3Q">
+                                <input type="image" src="{{ asset('ui/frontend/images/logo/donate_button.png') }}" width="148px" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button">
+                                <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                                </form>
+                
+                             
+                        </li>
+                        
+                    </div> 
+                    
+                    
+                </div>
+            </div>
         </div>
-      </div>
+        <div class="section-5 bg-madison context-dark novi-background" style="background-color:#002833;">
+            <div class="shell text-md-left">
+                <p class="">© <span class="copyright-year">{{ now()->year }}</span> All Rights Reserved Terms of Use and Privacy Policy.<span> Develop&nbsp;by <a href="https://shorturl.at/hkENZ" target="blank">Break-IT</a></span></p>
+            </div>
+        </div>
     </div>
-  </footer>
+</footer>
