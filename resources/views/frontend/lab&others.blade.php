441aa7e5c0f191c7befc11d4fc8d89c27f00004a
santo1989 <65486217+santo1989@users.noreply.github.com>
2022-05-30 17:45:40 +0600

    Image grid at laboratory page. #11

--- resources/views/frontend/lab&others.blade.php
+++ resources/views/frontend/lab&others.blade.php
@@ -1,5 +1,7 @@
 <x-frontend.layouts.master>
-<section class="section breadcrumb-modern context-dark parallax-container text-center" data-parallax-img="{{ asset('ui/frontend/images/parallax-03.jpg') }}">
+    
+
+<section class="section breadcrumb-modern context-dark parallax-container text-center" data-parallax-img="parallax-03.jpg">
         <div class="parallax-content section-30 section-sm-70">
             <div class="shell">
                 <h2 class="veil reveal-sm-block">Campus & Others</h2>
@@ -13,7 +15,7 @@
         </div>
     </section>
 
-   <section style="margin:150px;">
+   <section style="margin:30px; padding:0px 0px 20px 20px">
    <h3 class="veil reveal-lg-block" style="color:#002833;">Facilities of SBFNI Nursing Institute</h3>
    <div style="font-size:20px;">
        1. Scholarships for poor and meritorious students.<br>
@@ -29,9 +31,169 @@
        11. Adequate sports and recreational facilities.<br>
        12. Noise-free campus.<br>
    </div>
+</section>
+<section class="section novi-background bg-cover section-70 section-md-114 bg-default">
+    <div class="shell">
+        {{-- <h2>Grid Gallery</h2> --}}
+        <hr class="divider bg-madison">
+        <div class="offset-top-60">
+            <div class="range range-30 range-xs-center" data-lightgallery="group">
+                <div class="cell-xs-10 cell-sm-6 cell-md-4">
+                    <figure class="thumbnail-classic thumbnail-md">
+                        <div class="thumbnail-classic-img-wrap" ><img src="{{ asset('ui/frontend/images/gallary/img1.jpg') }}" alt="" width="370" height="370"> </div>
+                        <figcaption class="thumbnail-classic-caption text-center">
+                            {{-- <div>
+                                <h4 class="thumbnail-classic-title">Photo #1</h4>
+                            </div>
+                            <hr class="divider divider-sm">
+                            <p>Eposs messis in fortis revalia! Persuadere una ducunt ad audax aonides</p> --}}
+                            <div class="offset-top-20 veil reveal-lg-block"><a class="thumbnail-classic-link icon novi-icon icon-xxs fa-search-plus" href="{{ asset('ui/frontend/images/gallary/img1.jpg') }}" data-lightgallery="item" width="370" height="370"><img src="{{ asset('ui/frontend/images/gallary/img1.jpg') }}" alt="" width="370" height="370"></a></div>
+                        </figcaption>
+                    </figure>
+                </div>
 
-   <h3 class="veil reveal-lg-block" style="color:#002833; margin-top:40px;">Extra Facilities of SBFNI Nursing Institute</h3>
-   <p style="font-size:20px;">Students will not need to buy any books. There is also a set of high quality beds, reading tables, reading chairs and mattress pillows for the students staying in the hostel. 5% quota for poor and meritorious students is taught for free. Moreover, if a student can get very good result in 1st year final examination, then his next course fee is completely waived. Even many students who are very good at studying stay completely free. So there are many benefits to studying here. There is a nice management and board of directors who are very efficient and liberal. While pursuing Diploma in Nursing Science and Midwifery degree from SBF Nursing Institute, Lalmonirhat, there are many courses including Post Basic BSc in Nursing and MSc in Nursing or MPH. There are many job opportunities in this profession at home and abroad, including getting a doctorate degree in nursing while working. style="font-size:20px;">
+               
+
+                <div class="cell-xs-10 cell-sm-6 cell-md-4">
+                    <figure class="thumbnail-classic thumbnail-md">
+                        <div class="thumbnail-classic-img-wrap" ><img src="{{ asset('ui/frontend/images/gallary/img2.jpg') }}" alt="" width="370" height="370"> </div>
+                        <figcaption class="thumbnail-classic-caption text-center">
+                            {{-- <div>
+                                <h4 class="thumbnail-classic-title">Photo #1</h4>
+                            </div>
+                            <hr class="divider divider-sm">
+                            <p>Eposs messis in fortis revalia! Persuadere una ducunt ad audax aonides</p> --}}
+                            <div class="offset-top-20 veil reveal-lg-block"><a class="thumbnail-classic-link icon novi-icon icon-xxs fa-search-plus" href="{{ asset('ui/frontend/images/gallary/img2.jpg') }}" data-lightgallery="item" width="370" height="370"><img src="{{ asset('ui/frontend/images/gallary/img2.jpg') }}" alt="" width="370" height="370"></a></div>
+                        </figcaption>
+                    </figure>
+                </div>
+
+                <div class="cell-xs-10 cell-sm-6 cell-md-4">
+                    <figure class="thumbnail-classic thumbnail-md" >
+                        <div class="thumbnail-classic-img-wrap"><img src="{{ asset('ui/frontend/images/gallary/img4.jpg') }}" alt="" width="370" height="370"> </div>
+                        <figcaption class="thumbnail-classic-caption text-center">
+                            {{-- <div>
+                                <h4 class="thumbnail-classic-title">Photo #1</h4>
+                            </div>
+                            <hr class="divider divider-sm">
+                            <p>Eposs messis in fortis revalia! Persuadere una ducunt ad audax aonides</p> --}}
+                            <div class="offset-top-20 veil reveal-lg-block"><a class="thumbnail-classic-link icon novi-icon icon-xxs fa-search-plus" href="{{ asset('ui/frontend/images/gallary/img4.jpg') }}" data-lightgallery="item" width="370" height="370"><img src="{{ asset('ui/frontend/images/gallary/img4.jpg') }}" alt="" width="370" height="370"></a></div>
+                        </figcaption>
+                    </figure>
+                </div>
+
+                {{-- <div class="cell-xs-10 cell-sm-6 cell-md-4">
+                    <figure class="thumbnail-classic thumbnail-md">
+                        <div class="thumbnail-classic-img-wrap"><img src="images/portfolio/gallery-02-370x370.jpg" alt="" width="370" height="370"> </div>
+                        <figcaption class="thumbnail-classic-caption text-center">
+                            <div>
+                                <h4 class="thumbnail-classic-title">Photo #2</h4>
+                            </div>
+                            <hr class="divider divider-sm">
+                            <p>Est festus habena, cesaris. A falsis, luna salvus barcas. Talis exsul inter</p>
+                            <div class="offset-top-20 veil reveal-lg-block"><a class="thumbnail-classic-link icon novi-icon icon-xxs fa-search-plus" href="images/portfolio/gallery-02-370x370.jpg" data-lightgallery="item"><img src="images/portfolio/gallery-02-370x370.jpg" alt="" width="370" height="370"></a></div>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="cell-xs-10 cell-sm-6 cell-md-4">
+                    <figure class="thumbnail-classic thumbnail-md">
+                        <div class="thumbnail-classic-img-wrap"><img src="images/portfolio/gallery-03-370x370.jpg" alt="" width="370" height="370"> </div>
+                        <figcaption class="thumbnail-classic-caption text-center">
+                            <div>
+                                <h4 class="thumbnail-classic-title">Photo #3</h4>
+                            </div>
+                            <hr class="divider divider-sm">
+                            <p>Vae. Cur gallus experimentum? Devatios peregrinatione! Impositio cadunts</p>
+                            <div class="offset-top-20 veil reveal-lg-block"><a class="thumbnail-classic-link icon novi-icon icon-xxs fa-search-plus" href="images/portfolio/gallery-03-370x370.jpg" data-lightgallery="item"><img src="images/portfolio/gallery-03-370x370.jpg" alt="" width="370" height="370"></a></div>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="cell-xs-10 cell-sm-6 cell-md-4">
+                    <figure class="thumbnail-classic thumbnail-md">
+                        <div class="thumbnail-classic-img-wrap"><img src="images/portfolio/gallery-04-370x370.jpg" alt="" width="370" height="370"> </div>
+                        <figcaption class="thumbnail-classic-caption text-center">
+                            <div>
+                                <h4 class="thumbnail-classic-title">Photo #4</h4>
+                            </div>
+                            <hr class="divider divider-sm">
+                            <p>Est ferox domus, cesaris. Cur nix ridetis? Magister de nobilis assimilatio</p>
+                            <div class="offset-top-20 veil reveal-lg-block"><a class="thumbnail-classic-link icon novi-icon icon-xxs fa-search-plus" href="images/portfolio/gallery-04-370x370.jpg" data-lightgallery="item"><img src="images/portfolio/gallery-04-370x370.jpg" alt="" width="370" height="370"></a></div>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="cell-xs-10 cell-sm-6 cell-md-4">
+                    <figure class="thumbnail-classic thumbnail-md">
+                        <div class="thumbnail-classic-img-wrap"><img src="images/portfolio/gallery-05-370x370.jpg" alt="" width="370" height="370"> </div>
+                        <figcaption class="thumbnail-classic-caption text-center">
+                            <div>
+                                <h4 class="thumbnail-classic-title">Photo #5</h4>
+                            </div>
+                            <hr class="divider divider-sm">
+                            <p>A falsis, detrius secundus repressor. Ridetis sapienter ducunt ad audax</p>
+                            <div class="offset-top-20 veil reveal-lg-block"><a class="thumbnail-classic-link icon novi-icon icon-xxs fa-search-plus" href="images/portfolio/gallery-05-370x370.jpg" data-lightgallery="item"><img src="images/portfolio/gallery-05-370x370.jpg" alt="" width="370" height="370"></a></div>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="cell-xs-10 cell-sm-6 cell-md-4">
+                    <figure class="thumbnail-classic thumbnail-md">
+                        <div class="thumbnail-classic-img-wrap"><img src="images/portfolio/gallery-06-370x370.jpg" alt="" width="370" height="370"> </div>
+                        <figcaption class="thumbnail-classic-caption text-center">
+                            <div>
+                                <h4 class="thumbnail-classic-title">Photo #6</h4>
+                            </div>
+                            <hr class="divider divider-sm">
+                            <p>Historia, orgia, et finis. Adgium ferox nixus est. Hercle, classis dexter</p>
+                            <div class="offset-top-20 veil reveal-lg-block"><a class="thumbnail-classic-link icon novi-icon icon-xxs fa-search-plus" href="images/portfolio/gallery-06-370x370.jpg" data-lightgallery="item"><img src="images/portfolio/gallery-06-370x370.jpg" alt="" width="370" height="370"></a></div>
+                        </figcaption>
+                    </figure>
+                </div> --}}
+                {{-- <div class="cell-xs-10 cell-sm-6 cell-md-4">
+                    <figure class="thumbnail-classic thumbnail-md">
+                        <div class="thumbnail-classic-img-wrap"><img src="images/portfolio/gallery-07-370x370.jpg" alt="" width="370" height="370"> </div>
+                        <figcaption class="thumbnail-classic-caption text-center">
+                            <div>
+                                <h4 class="thumbnail-classic-title">Photo #7</h4>
+                            </div>
+                            <hr class="divider divider-sm">
+                            <p>Bromiums peregrinatione in avenio! A falsis, equiso peritus assimilatio</p>
+                            <div class="offset-top-20 veil reveal-lg-block"><a class="thumbnail-classic-link icon novi-icon icon-xxs fa-search-plus" href="images/portfolio/gallery-07-370x370.jpg" data-lightgallery="item"><img src="images/portfolio/gallery-07-370x370.jpg" alt="" width="370" height="370"></a></div>
+                        </figcaption>
+                    </figure>
+                </div> --}}
+                {{-- <div class="cell-xs-10 cell-sm-6 cell-md-4">
+                    <figure class="thumbnail-classic thumbnail-md">
+                        <div class="thumbnail-classic-img-wrap"><img src="images/portfolio/gallery-08-370x370.jpg" alt="" width="370" height="370"> </div>
+                        <figcaption class="thumbnail-classic-caption text-center">
+                            <div>
+                                <h4 class="thumbnail-classic-title">Photo #8</h4>
+                            </div>
+                            <hr class="divider divider-sm">
+                            <p>Pars tolerares, tanquam brevis silva. Eheu, cursus! Glutens favere, tanquam</p>
+                            <div class="offset-top-20 veil reveal-lg-block"><a class="thumbnail-classic-link icon novi-icon icon-xxs fa-search-plus" href="images/portfolio/gallery-08-370x370.jpg" data-lightgallery="item"><img src="images/portfolio/gallery-08-370x370.jpg" alt="" width="370" height="370"></a></div>
+                        </figcaption>
+                    </figure>
+                </div> --}}
+                {{-- <div class="cell-xs-10 cell-sm-6 cell-md-4">
+                    <figure class="thumbnail-classic thumbnail-md">
+                        <div class="thumbnail-classic-img-wrap"><img src="images/portfolio/gallery-09-370x370.jpg" alt="" width="370" height="370"> </div>
+                        <figcaption class="thumbnail-classic-caption text-center">
+                            <div>
+                                <h4 class="thumbnail-classic-title">Photo #9</h4>
+                            </div>
+                            <hr class="divider divider-sm">
+                            <p>Eheu. Sunt parses aperto audax, lotus brodiumes. Camerarius idoleums</p>
+                            <div class="offset-top-20 veil reveal-lg-block"><a class="thumbnail-classic-link icon novi-icon icon-xxs fa-search-plus" href="images/portfolio/gallery-09-370x370.jpg" data-lightgallery="item"><img src="images/portfolio/gallery-09-370x370.jpg" alt="" width="370" height="370"></a></div>
+                        </figcaption>
+                    </figure>
+                </div> --}}
+            </div>
+        </div>
+    </div>
+</section>
+
+<section style="margin:5px;padding:0px 20px 20px 20px">
+   <h3 class="veil reveal-lg-block" style="color:#002833; margin-top:40px; ">Extra Facilities of SBFNI Nursing Institute</h3>
+   <p style="font-size:20px;">Students will not need to buy any books. There is also a set of high quality beds, reading tables, reading chairs and mattress pillows for the students staying in the hostel. 5% quota for poor and meritorious students is taught for free. Moreover, if a student can get very good result in 1st year final examination, then his next course fee is completely waived. Even many students who are very good at studying stay completely free. So there are many benefits to studying here. There is a nice management and board of directors who are very efficient and liberal. While pursuing Diploma in Nursing Science and Midwifery degree from SBF Nursing Institute, Lalmonirhat, there are many courses including Post Basic BSc in Nursing and MSc in Nursing or MPH. There are many job opportunities in this profession at home and abroad, including getting a doctorate degree in nursing while working.
 
    </p>
    </section>
